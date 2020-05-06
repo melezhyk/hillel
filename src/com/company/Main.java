c06b9895d40e18ddd615725e89cd071464f5e90c
melezhyk <melezhikalyona@gmail.com>
2020-05-06 16:40:48 +0300

    hello

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	System.out.println( dividesByTree (25) );
+
+    }
+    static boolean dividesByTree (int a){
+        return (a%3==0);
+    }
+}
